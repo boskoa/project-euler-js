f34f985aee030bffd431ef393612a3dab834a49c
boskoa <bosko.ap@gmail.com>
2025-07-27 11:30:11 +0200

    Adding problem 01

---
+++ 01-multiples_of_3_or_5.js
@@ -0,0 +1,12 @@
+function getMultiples(range) {
+  let sum = 0;
+  for (let i = 1; i < range; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+
+  return sum;
+}
+
+console.log(getMultiples(1000));
